0d71145c0d5e215469ba8c886b55084a1008a9f6
zhangyang06 <zhangyang06@bilibili.com>
2023-08-04 15:49:38 +0800

    [feat] 初始化整个工程

---
+++ joker/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("multiplatform") version "1.8.0"
+}
+
+
+kotlin {
+    jvm {
+        jvmToolchain(11)
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/central/") }
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "binary-compatibility-validator" -> {
+                    useModule("org.jetbrains.kotlinx:binary-compatibility-validator:${requested.version}")
+                }
+                "com.android.application", "com.android.library" -> {
+                    useModule("com.android.tools.build:gradle:${requested.version}")
+                }
+            }
+        }
+    }
+}
+include(":joker")
